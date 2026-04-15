f433f24548c6f6c45ebbdfd7d407a8d1bbf20ac3
TMAdevby <mtipun@mail.ru>
2026-04-15 18:19:57 +0300

    initial

---
+++ src/main/java/com/example/netotask50microservicesproject/NetoTask50MicroservicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.netotask50microservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetoTask50MicroservicesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetoTask50MicroservicesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netotask50microservicesproject/NetoTask50MicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netotask50microservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetoTask50MicroservicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
