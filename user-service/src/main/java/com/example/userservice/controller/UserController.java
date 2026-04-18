1a797d4ae66d6309fa1f3d8969e369695b2720d5
TMAdevby <mtipun@mail.ru>
2026-04-18 16:46:03 +0300

    user service3

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.model.User;
+import com.example.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") Long id) {
+
+        Optional<User> userOptional = userService.findById(id);
+
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok(userOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -1,4 +1,41 @@
 package com.example.userservice.service;
 
+import com.example.userservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final Map<Long, User> userDatabase = new HashMap<>();
+
+    public UserService() {
+        userDatabase.put(1L, new User(
+                1L,
+                "Иванов",
+                "Иван",
+                "Иванович",
+                "г. Москва, ул. Ленина, д. 10",
+                "+7-900-123-45-67",
+                "ivan@example.com"
+        ));
+
+        userDatabase.put(2L, new User(
+                2L,
+                "Петров",
+                "Пётр",
+                "Петрович",
+                "г. Санкт-Петербург, ул. Невский, д. 25",
+                "+7-900-987-65-43",
+                "petr@example.com"
+        ));
+    }
+
+    public Optional<User> findById(Long id) {
+        // Optional.ofNullable() создаёт Optional, который может быть пустым
+        return Optional.ofNullable(userDatabase.get(id));
+    }
 }
