1de1fe1d187cfb3319ccc9360d616cd7ccfb1df9
TMAdevby <mtipun@mail.ru>
2026-04-19 16:17:24 +0300

    order 3

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,4 +1,25 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.model.Order;
+import com.example.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/by-user/{user}")
+    public List<Order> getOrdersByUser(@PathVariable("user") Long userId) {
+
+        return orderService.findByUserId(userId);
+    }
 }
